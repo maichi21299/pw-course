32a516c651b16f29a71aa328831599bfb2656a81
Mai Chi <maichidao99@gmail.com>
2026-06-24 23:08:30 +0700

    Hoàn thành BT2

---
+++ BT2/tests/lesson-02/ex4.js
@@ -0,0 +1,8 @@
+const height = 160;
+const oddpart = height - 100;
+
+const cannanglytuong = oddpart * 9 / 10;
+const cannangtoida = oddpart;
+const cantangtoithieu = oddpart * 8 / 10;
+
+console.log("Chiều cao", height, "cm" + "Cân nặng lý tưởng:", cannanglytuong, "kg" + "Cân nặng tối đa:", cannangtoida, "kg" + "Cân nặng tối thiểu:", cantangtoithieu, "kg");
---
+++ BT2/tests/lesson-02/ex6.js
@@ -0,0 +1,3 @@
+for (let i = 12; i <= 30; i++) {
+    console.log("Các giá trị hiển thị là:", i);
+}
